88f4709b7fb732bf04163257a2c7e8ffb031897f
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-08-25 16:20:20 -0300

    feat(Livro): criado repositories para livro

---
+++ bibliotecaBack/src/repositories/LivroRepositories.js
@@ -0,0 +1,210 @@
+import prisma from '../database/prisma.js'
+
+class LivroRepositories {
+     async visualizar () {
+          const result = await prisma.livro_autor.findMany({
+               select: {
+                    livro: {
+                         select: {
+                              id: true,
+                              isbn: true,
+                              titulo: true,
+                              ano_publicacao: true,
+                              edicao: true,
+                              editora: true,
+                              categoria: true,
+                              descricao: true
+                         }
+                    },
+                    autor: true
+               }
+          })
+
+          return result
+     }
+     async buscarPorID (livro) {
+          const result = await prisma.livro_autor.findUnique({
+               where: {
+                    id: livro.id
+               },
+               select: {
+                    livro: {
+                         select: {
+                              id: true,
+                              isbn: true,
+                              titulo: true,
+                              ano_publicacao: true,
+                              edicao: true,
+                              editora: true,
+                              categoria: true,
+                              descricao: true
+                         }
+                    },
+                    autor: true
+               }
+          })
+
+          return result
+     }
+     async buscarPorIsbn (livro) {
+          const result = await prisma.livro.findUnique({
+               where: {
+                    isbn: livro.isbn
+               }
+          })
+
+          return result
+     }
+     async adicionar (livro) {
+          const result = await prisma.$transaction(async (fx) => {
+               const book = await fx.livro.create({
+                    data: {
+                         isbn: livro.isbn,
+                         titulo: livro.titulo,
+                         ano_publicacao: livro.ano_publicacao,
+                         edicao: livro.edicao,
+                         editora: livro.editora,
+                         categoria_id: livro.categoria_id,
+                         descricao: livro.descricao
+                    }, select : {
+                         id: true,
+                         isbn: true,
+                         titulo: true,
+                         ano_publicacao: true,
+                         edicao: true,
+                         editora: true,
+                         categoria: true,
+                         descricao: true
+                    }
+               })
+
+               const livro_autor = await fx.livro_autor.create({
+                    data: {
+                         livro_id: book.id,
+                         autor_id: livro.autor_id
+                    }, select: {
+                         autor: true
+                    }
+               })
+
+               return {
+                    id: livro_autor.id,
+                    livro: {
+                         id: book.id,
+                         isbn: book.isbn,
+                         titulo: book.titulo,
+                         ano_publicacao: book.ano_publicacao,
+                         edicao: book.edicao,
+                         editora: book.editora,
+                         categoria: book.categoria,
+                         descricao: book.descricao,
+                    },
+                    autor: {
+                         id: livro_autor.autor.id,
+                         nome: livro_autor.autor.nome,
+                         nascionalidade: livro_autor.autor.nascionalidade,
+                         data_nascimento: livro_autor.autor.data_nascimento
+                    }
+               }
+          })
+
+          return result
+     }
+     async atualizar (livro) {
+          const result = await prisma.$transaction(async (fx) => {
+               const livro_autor = await fx.livro_autor.update({
+                    where: {
+                         id: livro.id
+                    },
+                    data: {
+                         autor_id: livro.autor_id
+                    }, select: {
+                         autor: true,
+                         livro: true
+                    }
+               })
+               
+               const book = await fx.livro.update({
+                    where: {
+                         id: livro_autor.livro.id
+                    },
+                    data: {
+                         isbn: livro.isbn,
+                         titulo: livro.titulo,
+                         ano_publicacao: livro.ano_publicacao,
+                         edicao: livro.edicao,
+                         editora: livro.editora,
+                         categoria_id: livro.categoria_id,
+                         descricao: livro.descricao
+                    }, select : {
+                         id: true,
+                         isbn: true,
+                         titulo: true,
+                         ano_publicacao: true,
+                         edicao: true,
+                         editora: true,
+                         categoria: true,
+                         descricao: true
+                    }
+               })
+
+               return {
+                    id: livro_autor.id,
+                    livro: {
+                         id: book.id,
+                         isbn: book.isbn,
+                         titulo: book.titulo,
+                         ano_publicacao: book.ano_publicacao,
+                         edicao: book.edicao,
+                         editora: book.editora,
+                         categoria: book.categoria,
+                         descricao: book.descricao,
+                    },
+                    autor: {
+                         id: livro_autor.autor.id,
+                         nome: livro_autor.autor.nome,
+                         nascionalidade: livro_autor.autor.nascionalidade,
+                         data_nascimento: livro_autor.autor.data_nascimento
+                    }
+               }
+          })
+
+          return result
+     }
+     async deletar (livro) {
+          const result = await prisma.$transaction(async (fx) => {
+               const livro_autor = await fx.livro_autor.delete({
+                    where: {
+                         id: livro.id
+                    },
+                    select: {
+                         id: true,
+                         livro: true
+                    }
+               })
+               const book = await fx.livro.delete({
+                    where: {
+                         id: livro_autor.livro.id 
+                    },
+                    select : {
+                         id: true,
+                         isbn: true,
+                         titulo: true
+                    }
+               })
+
+               return {
+                    id: livro_autor.id,
+                    livro: {
+                         id: book.id,
+                         isbn: book.isbn,
+                         titulo: book.titulo
+                    }
+               }
+          })
+
+          return result
+     }
+}
+
+export default LivroRepositories
