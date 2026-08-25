ada6f6fb2eeacd5fe408ec30cae87a1b08b527de
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-08-25 16:20:02 -0300

    feat(Categoria): criado repositories para categoria

---
+++ bibliotecaBack/src/repositories/CategoriaRepositories.js
@@ -0,0 +1,52 @@
+import prisma from '../database/prisma.js'
+
+class CategoriaRepositories {
+     async visualizar () {
+          const result = await prisma.categoria.findMany()
+
+          return result
+     }
+     async buscarPorID (categoria) {
+          const result = await prisma.categoria.findUnique({
+               where: {
+                    id: categoria.id
+               }
+          })
+
+          return result
+     }
+     async adicionar (categoria) {
+          const result = await prisma.categoria.create({
+               data: {
+                    nome: categoria.nome,
+                    descricao: categoria.descricao
+               }
+          })
+
+          return result
+     }
+     async atualizar (categoria) {
+          const result = await prisma.categoria.update({
+               where: {
+                    id: categoria.id
+               },
+               data: {
+                    nome: categoria.nome,
+                    descricao: categoria.descricao
+               }
+          })
+
+          return result
+     }
+     async deletar (categoria) {
+          const result = await prisma.categoria.delete({
+               where: {
+                    id: categoria.id
+               }
+          })
+
+          return result
+     }
+}
+
+export default CategoriaRepositories
