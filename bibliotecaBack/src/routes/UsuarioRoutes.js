a63d1aec4b8ea570cf895b67c9f2cd56d6177c64
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-02 13:56:52 -0300

    feat: implementa o uso do asyncHandler nas rotas

--- bibliotecaBack/src/routes/UsuarioRoutes.js
+++ bibliotecaBack/src/routes/UsuarioRoutes.js
@@ -2,14 +2,15 @@ import express from 'express'
 import UsuarioControllers from '../controllers/UsuarioControllers.js'
 import AuthMiddlewares from '../middlewares/AuthMiddlewares.js'
 import AdminMiddlewares from '../middlewares/AdminMiddlewares.js'
+import asyncHanlder from '../middlewares/AsyncHandler.js'
 
 const usuario_controllers = new UsuarioControllers()
 const UsuarioRoutes = express.Router()
 
-UsuarioRoutes.get('/', usuario_controllers.visualizar)
-UsuarioRoutes.get('/:id', usuario_controllers.buscarPorID)
-UsuarioRoutes.post('/', AuthMiddlewares, AdminMiddlewares, usuario_controllers.adicionar)
-UsuarioRoutes.put('/:id', AuthMiddlewares, AdminMiddlewares, usuario_controllers.atualizar)
-UsuarioRoutes.patch('/:id', AuthMiddlewares, AdminMiddlewares, usuario_controllers.alterarAtivo)
+UsuarioRoutes.get('/', asyncHanlder(usuario_controllers.visualizar))
+UsuarioRoutes.get('/:id', asyncHanlder(usuario_controllers.buscarPorID))
+UsuarioRoutes.post('/', AuthMiddlewares, AdminMiddlewares, asyncHanlder(usuario_controllers.adicionar))
+UsuarioRoutes.put('/:id', AuthMiddlewares, AdminMiddlewares, asyncHanlder(usuario_controllers.atualizar))
+UsuarioRoutes.patch('/:id', AuthMiddlewares, AdminMiddlewares, asyncHanlder(usuario_controllers.alterarAtivo))
 
 export default UsuarioRoutes
